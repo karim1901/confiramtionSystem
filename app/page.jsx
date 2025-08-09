fd6205d18f5ec34f72582d7dfe6119683bc24a0b
karim ossama <karimossama591@gmail.com>
2025-08-09 17:13:16 +0100

    seller

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const page = () => {
   const router = useRouter()
   return (
-    <div className='flex flex-col justify-center items-center gap-4'>
+    <div className='flex flex-col justify-center items-center gap-4 h-screen'>
       <p className='text-blue-400  ' onClick={()=>{router.push("/sc")}}>Employee</p>
       <p className='text-blue-400  ' onClick={()=>{router.push("/seller")}}>seller</p>
     </div>
